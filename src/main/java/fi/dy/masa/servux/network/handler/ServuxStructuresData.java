a3cd33462f2daa0c70b0b1bb0dff61f043aae1de
Sakura Ryoko <sakura@ryoko.soy>
2024-06-10 15:41:35 -0400

    1.21 rc1

---
+++ src/main/java/fi/dy/masa/servux/network/handler/ServuxStructuresData.java
@@ -0,0 +1,13 @@
+package fi.dy.masa.servux.network.handler;
+
+import net.minecraft.nbt.NbtCompound;
+import net.minecraft.nbt.NbtElement;
+import net.minecraft.nbt.NbtList;
+
+public class ServuxStructuresData
+{
+    private NbtElement nbtElement;
+    private NbtCompound nbtCompound;
+    private NbtList nbtList;
+    private int packetType;
+}
--- src/main/java/fi/dy/masa/servux/network/handler/ServuxStructuresPayload.java
+++ src/main/java/fi/dy/masa/servux/network/handler/ServuxStructuresPayload.java
@@ -13,10 +13,16 @@ public record ServuxStructuresPayload(NbtCompound data) implements CustomPayload
     public static final Id<ServuxStructuresPayload> TYPE = new Id<>(ServuxStructuresHandler.CHANNEL_ID);
     public static final PacketCodec<PacketByteBuf, ServuxStructuresPayload> CODEC = CustomPayload.codecOf(ServuxStructuresPayload::write, ServuxStructuresPayload::new);
 
-    public ServuxStructuresPayload(PacketByteBuf buf) { this(buf.readNbt()); }
+    public ServuxStructuresPayload(PacketByteBuf input)
+    {
+        this(input.readNbt());
+    }
 
-    private void write(PacketByteBuf buf) { buf.writeNbt(data); }
+    private void write(PacketByteBuf output)
+    {
+        output.writeNbt(data);
+    }
 
     @Override
-    public Id<? extends CustomPayload> getId() { return TYPE; }
+    public Id<ServuxStructuresPayload> getId() { return TYPE; }
 }
