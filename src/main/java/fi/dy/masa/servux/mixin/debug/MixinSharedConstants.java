c5174ec9ce26b350f1169a34bcc83454c2746534
Sakura Ryoko <sakura@ryoko.soy>
2024-11-06 17:59:59 -0500

    port to 24w45a

--- src/main/java/fi/dy/masa/servux/mixin/debug/MixinSharedConstants.java
+++ src/main/java/fi/dy/masa/servux/mixin/debug/MixinSharedConstants.java
@@ -4,11 +4,6 @@ import net.minecraft.SharedConstants;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Mutable;
 import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-
-import fi.dy.masa.servux.dataproviders.DebugDataProvider;
 
 @Mixin(SharedConstants.class)
 public abstract class MixinSharedConstants
