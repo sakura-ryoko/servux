e5a125a4252a16a893c82d79d2df067d9716f5ea
Sakura Ryoko <sakura@ryoko.soy>
2024-11-30 11:31:21 -0500

    Remove crowdin

--- src/main/java/fi/dy/masa/servux/dataproviders/ServuxConfigProvider.java
+++ src/main/java/fi/dy/masa/servux/dataproviders/ServuxConfigProvider.java
@@ -27,7 +27,7 @@ public class ServuxConfigProvider extends DataProviderBase
     private final ServuxIntSetting easyPlacePermissionLevel = new ServuxIntSetting(this, "permission_level_easy_place", 0, 4, 0);
     private final ServuxStringSetting defaultLanguage = new ServuxStringSetting(this, "default_language",
         i18nLang.DEFAULT_LANG,
-        List.of("en_us", "zh_cn", "zh_tw", "ar_sa", "es_es", "hi_in", "ja_jp", "ko_kr", "pl_pl", "ru_ru", "uk_ua"), false)
+        List.of("en_us", "zh_cn"), false)
     {
         @Override
         public void setValueNoCallback(String value)
