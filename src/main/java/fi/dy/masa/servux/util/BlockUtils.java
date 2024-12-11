d86a102f37501d1b2d25e12730001247d5e22cbf
Sakura Ryoko <sakura@ryoko.soy>
2024-12-10 22:15:28 -0500

    fix Placement Handler for Easy Place Protocol

--- src/main/java/fi/dy/masa/servux/util/BlockUtils.java
+++ src/main/java/fi/dy/masa/servux/util/BlockUtils.java
@@ -1,49 +1,51 @@
 package fi.dy.masa.servux.util;
 
+import java.util.Optional;
+import javax.annotation.Nullable;
+
 import net.minecraft.block.BlockState;
 import net.minecraft.state.property.EnumProperty;
 import net.minecraft.state.property.Properties;
 import net.minecraft.state.property.Property;
 import net.minecraft.util.math.Direction;
 
-import javax.annotation.Nullable;
-
 public class BlockUtils
 {
     /**
      * Returns the first PropertyDirection property from the provided state, if any.
-     * @param state
+     *
+     * @param state ()
      * @return the first PropertyDirection, or null if there are no such properties
      */
     @SuppressWarnings("unchecked")
-    @Nullable
-    public static EnumProperty<Direction> getFirstDirectionProperty(BlockState state)
+    public static Optional<EnumProperty<Direction>> getFirstDirectionProperty(BlockState state)
     {
         for (Property<?> prop : state.getProperties())
         {
-            if (prop instanceof EnumProperty<?> enumProperty)
+            if (prop instanceof EnumProperty<?> ep)
             {
-                if (enumProperty.getType().equals(Direction.class))
+                if (ep.getType().equals(Direction.class))
                 {
-                    return (EnumProperty<Direction>) enumProperty;
+                    return Optional.of((EnumProperty<Direction>) ep);
                 }
             }
         }
 
-        return null;
+        return Optional.empty();
     }
 
     /**
      * Returns the EnumFacing value of the first found PropertyDirection
      * type blockstate property in the given state, if any.
      * If there are no PropertyDirection properties, then null is returned.
-     * @param state
-     * @return
+     *
+     * @param state ()
+     * @return ()
      */
-    @Nullable
-    public static Direction getFirstPropertyFacingValue(BlockState state)
+    public static Optional<Direction> getFirstPropertyFacingValue(BlockState state)
     {
-        return getPropertyFacingValue(state);
+        Optional<EnumProperty<Direction>> propOptional = getFirstDirectionProperty(state);
+        return propOptional.map((directionProperty) -> Direction.byId(((Direction) state.get(directionProperty)).getId()));
     }
 
     @Nullable
