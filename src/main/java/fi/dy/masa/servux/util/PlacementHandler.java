48852f34c6a2c355bdac053fc93197c360ee6e5d
Sakura Ryoko <sakura@ryoko.soy>
2024-12-16 22:29:11 -0500

    Fix Easy Place V3 for "AXIS" blocks, such as logs, and frog lights.

--- src/main/java/fi/dy/masa/servux/util/PlacementHandler.java
+++ src/main/java/fi/dy/masa/servux/util/PlacementHandler.java
@@ -100,8 +100,8 @@ public class PlacementHandler
         {
             for (Property<?> p : propList)
             {
-                //if (((p instanceof EnumProperty<?> ep) && ep.getType().equals(Direction.class) == false) &&
-                if (property.isPresent() && !property.get().equals(p) &&
+                if ((property.isPresent() && !property.get().equals(p)) ||
+                    (property.isEmpty()) &&
                     WHITELISTED_PROPERTIES.contains(p))
                 {
                     @SuppressWarnings("unchecked")
