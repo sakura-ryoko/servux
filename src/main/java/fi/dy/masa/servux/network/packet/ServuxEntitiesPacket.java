518984d3425fb3a3b795fc27089dccf097e345a6
Sakura Ryoko <sakura@ryoko.soy>
2025-01-16 21:21:05 -0500

    bump fapi network version

--- src/main/java/fi/dy/masa/servux/network/packet/ServuxEntitiesPacket.java
+++ src/main/java/fi/dy/masa/servux/network/packet/ServuxEntitiesPacket.java
@@ -3,17 +3,16 @@ package fi.dy.masa.servux.network.packet;
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 import io.netty.buffer.Unpooled;
+
 import net.minecraft.nbt.NbtCompound;
 import net.minecraft.nbt.NbtSizeTracker;
 import net.minecraft.network.PacketByteBuf;
 import net.minecraft.network.codec.PacketCodec;
 import net.minecraft.network.packet.CustomPayload;
 import net.minecraft.util.math.BlockPos;
+
 import fi.dy.masa.servux.Servux;
 import fi.dy.masa.servux.network.IServerPayloadData;
-import net.minecraft.util.math.ChunkPos;
-
-import java.util.List;
 
 public class ServuxEntitiesPacket implements IServerPayloadData
 {
@@ -262,7 +261,8 @@ public class ServuxEntitiesPacket implements IServerPayloadData
                 // Write Packet Buffer (Slice)
                 try
                 {
-                    output.writeBytes(this.buffer.readBytes(this.buffer.readableBytes()));
+                    PacketByteBuf serverReplay = new PacketByteBuf(this.buffer.copy());
+                    output.writeBytes(serverReplay.readBytes(serverReplay.readableBytes()));
                 }
                 catch (Exception e)
                 {
