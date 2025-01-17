84f195321b3028c7859d318d45e0586bb184803f
Sakura Ryoko <sakura@ryoko.soy>
2025-01-16 22:06:12 -0500

    Additional Network fixes

--- src/main/java/fi/dy/masa/servux/network/ServuxBuf.java
+++ src/main/java/fi/dy/masa/servux/network/ServuxBuf.java
@@ -3,6 +3,7 @@ package fi.dy.masa.servux.network;
 import io.netty.buffer.ByteBuf;
 import net.minecraft.network.PacketByteBuf;
 
+@Deprecated(forRemoval = true)
 public class ServuxBuf extends PacketByteBuf
 {
     public ServuxBuf(ByteBuf parent)
