ab76d8778b83d83b7e2381ab9bdb94c79d57499b
Sakura Ryoko <sakura@ryoko.soy>
2025-02-02 23:34:55 -0500

    Adjust fromPacket for PacketSplitter code

--- src/main/java/fi/dy/masa/servux/network/packet/ServuxHudPacket.java
+++ src/main/java/fi/dy/masa/servux/network/packet/ServuxHudPacket.java
@@ -177,8 +177,12 @@ public class ServuxHudPacket implements IServerPayloadData
                 // Write Packet Buffer (Slice)
                 try
                 {
+                    /*
                     PacketByteBuf serverReplay = new PacketByteBuf(this.buffer.copy());
                     output.writeBytes(serverReplay.readBytes(serverReplay.readableBytes()));
+                     */
+
+                    output.writeBytes(this.buffer.copy());
                 }
                 catch (Exception e)
                 {
