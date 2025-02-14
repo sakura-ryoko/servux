8d86f052001702008b11e44059528d8ea0f31e39
Sakura Ryoko <sakura@ryoko.soy>
2025-02-14 12:31:48 -0500

    fully fix easy placement handler

--- src/main/java/fi/dy/masa/servux/util/PlacementHandler.java
+++ src/main/java/fi/dy/masa/servux/util/PlacementHandler.java
@@ -106,13 +106,15 @@ public class PlacementHandler
         {
             for (Property<?> p : propList)
             {
+
+                /*
                 if ((property.isPresent() && !property.get().equals(p)) ||
                     (property.isEmpty()) &&
                     WHITELISTED_PROPERTIES.contains(p))
                     //WHITELISTED_PROPERTIES.contains(p) &&
                     //!BLACKLISTED_PROPERTIES.contains(p))
+                 */
 
-                /*
                 if (property.isPresent() && property.get().equals(p))
                 {
                     //System.out.printf("[PHv3] skipping prot val: 0x%08X [Property %s]\n", protocolValue, p.getName());
