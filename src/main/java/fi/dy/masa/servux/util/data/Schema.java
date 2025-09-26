0f3106c5958fd75b394a5d06bfa8592759d60786
Sakura Ryoko <sakura@ryoko.soy>
2025-09-26 12:24:47 -0400

    Bump Schema.java
    Update Schema.java

--- src/main/java/fi/dy/masa/servux/util/data/Schema.java
+++ src/main/java/fi/dy/masa/servux/util/data/Schema.java
@@ -21,8 +21,16 @@ public enum Schema implements StringIdentifiable
 {
     // TODO --> Add Schema Versions to this as versions get released
     // Minecraft Data Versions
-    SCHEMA_FUTURE  (9999, "FUTURE"),
-    SCHEMA_1_21_06 (4435, "1.21.6"),    // pre4
+	SCHEMA_FUTURE  (9999, "FUTURE"),
+	SCHEMA_25W46A  (4662, "25w46a"),
+	SCHEMA_25W41A  (4657, "25w41a"),
+	SCHEMA_1_21_10 (4556, "1.21.10"),
+	SCHEMA_1_21_09 (4554, "1.21.9"),
+    SCHEMA_25W36B  (4546, "25w36b"),
+	SCHEMA_25W31A  (4534, "25w31a"),
+    SCHEMA_1_21_08 (4440, "1.21.8"),
+    SCHEMA_1_21_07 (4438, "1.21.7"),
+    SCHEMA_1_21_06 (4435, "1.21.6"),
     SCHEMA_25W21A  (4429, "25w21a"),
     SCHEMA_25W16A  (4423, "25w16a"),
     SCHEMA_1_21_05 (4325, "1.21.5"),
