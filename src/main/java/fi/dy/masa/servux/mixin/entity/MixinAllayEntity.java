8ba6dfa3e53a1a182c0dc074a796e9b8c3d7099d
Sakura Ryoko <sakura@ryoko.soy>
2025-09-29 22:47:40 -0400

    fix it

--- src/main/java/fi/dy/masa/servux/mixin/entity/MixinAllayEntity.java
+++ src/main/java/fi/dy/masa/servux/mixin/entity/MixinAllayEntity.java
@@ -1,20 +1,24 @@
 package fi.dy.masa.servux.mixin.entity;
 
+import net.minecraft.entity.ai.brain.Brain;
 import net.minecraft.entity.passive.AllayEntity;
 import net.minecraft.world.GameRules;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
 import fi.dy.masa.servux.dataproviders.EntitiesDataProvider;
 
 @Mixin(AllayEntity.class)
-public class MixinAllayEntity
+public abstract class MixinAllayEntity
 {
+//	@Shadow public abstract Brain<AllayEntity> getBrain();
+
 	@Redirect(method = "canGather",
 	          at = @At(value = "INVOKE",
 	                   target = "Lnet/minecraft/world/GameRules;getBoolean(Lnet/minecraft/world/GameRules$Key;)Z"))
-	private boolean servux$fixAllayGathering(GameRules instance, GameRules.Key<GameRules.BooleanRule> rule)
+	private boolean servux$fixAllayGathering1(GameRules instance, GameRules.Key<GameRules.BooleanRule> rule)
 	{
 		if (EntitiesDataProvider.INSTANCE.hasFixAllayGathering())
 		{
@@ -23,4 +27,15 @@ public class MixinAllayEntity
 
 		return instance.getBoolean(GameRules.DO_MOB_GRIEFING);
 	}
+
+//	@Inject(method = "isItemPickupCoolingDown",
+//	        at = @At("RETURN"), cancellable = true)
+//	private void servux$fixAllayGathering2(CallbackInfoReturnable<Boolean> cir)
+//	{
+//		if (EntitiesDataProvider.INSTANCE.hasFixAllayGathering() &&
+//			cir.getReturnValue())
+//		{
+//			cir.setReturnValue(false);
+//		}
+//	}
 }
