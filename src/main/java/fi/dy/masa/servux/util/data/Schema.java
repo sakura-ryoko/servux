ce18b92275d3136b822989d02a90de09e8625bc6
Sakura Ryoko <sakura@ryoko.soy>
2025-12-09 19:39:44 -0500

    Bump Release, Luck Perms API

--- src/main/java/fi/dy/masa/servux/util/data/Schema.java
+++ src/main/java/fi/dy/masa/servux/util/data/Schema.java
@@ -22,7 +22,7 @@ public enum Schema implements StringRepresentable
     // TODO --> Add Schema Versions to this as versions get released
     // Minecraft Data Versions
 	SCHEMA_FUTURE  (9999, "FUTURE"),
-	SCHEMA_1_21_11 (4670, "1.21.11-rc3"),
+	SCHEMA_1_21_11 (4671, "1.21.11"),
 	SCHEMA_25W46A  (4662, "25w46a"),
 	SCHEMA_25W41A  (4657, "25w41a"),
 	SCHEMA_1_21_10 (4556, "1.21.10"),
