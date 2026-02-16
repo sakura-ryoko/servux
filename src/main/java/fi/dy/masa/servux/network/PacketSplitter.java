a119c97fa42c03c59fa6865261fe425dc752784c
Sakura Ryoko <sakura@ryoko.soy>
2026-02-15 22:40:27 -0500

    Align Packet Splitter with MaLiLib.

--- src/main/java/fi/dy/masa/servux/network/PacketSplitter.java
+++ src/main/java/fi/dy/masa/servux/network/PacketSplitter.java
@@ -23,8 +23,8 @@ public class PacketSplitter
     public static final int MAX_PAYLOAD_PER_PACKET_S2C = MAX_TOTAL_PER_PACKET_S2C - 5;
     public static final int MAX_TOTAL_PER_PACKET_C2S = 32767;
     public static final int MAX_PAYLOAD_PER_PACKET_C2S = MAX_TOTAL_PER_PACKET_C2S - 5;
-    public static final int DEFAULT_MAX_RECEIVE_SIZE_C2S = 1048576;
-    public static final int DEFAULT_MAX_RECEIVE_SIZE_S2C = 16777216;
+    public static final int DEFAULT_MAX_RECEIVE_SIZE_C2S = 16777216;
+    public static final int DEFAULT_MAX_RECEIVE_SIZE_S2C = 67108864;
 
     private static final Map<Long, ReadingSession> READING_SESSIONS = new HashMap<>();
 
