f47d37433c04be601b69daf36eb98a0b1dafccab
Sakura Ryoko <sakura@ryoko.soy>
2026-09-02 22:15:00 -0400

    fix: Mixin problem that only showed itself outside an IDE.

--- src/main/java/fi/dy/masa/servux/mixin/server/MixinMain.java
+++ src/main/java/fi/dy/masa/servux/mixin/server/MixinMain.java
@@ -1,27 +1,31 @@
 package fi.dy.masa.servux.mixin.server;
 
-import java.nio.file.Path;
-import com.llamalad7.mixinextras.sugar.Local;
+import java.nio.file.Paths;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+
 import net.minecraft.core.RegistryAccess;
 import net.minecraft.server.Main;
+import net.minecraft.world.level.storage.LevelStorageSource;
+import net.minecraft.world.level.storage.WorldData;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import fi.dy.masa.servux.dataproviders.DataProviderManager;
 
 @Mixin(Main.class)
 public class MixinMain
 {
-    @Inject(method = "main", at = @At(value = "INVOKE",
-            target = "Lnet/minecraft/world/level/storage/LevelStorageSource$LevelStorageAccess;saveDataTag(Lnet/minecraft/core/RegistryAccess;Lnet/minecraft/world/level/storage/WorldData;)V",
-            shift = At.Shift.AFTER))
-    private static void servux_onCaptureImmutable(String[] args, CallbackInfo ci,
-                                                  @Local(name = "frozen") RegistryAccess.Frozen frozen,
-                                                  @Local(name = "path2") Path settingsFile)
+    // Apparently, 1.21.11 breaks here
+    @WrapOperation(method = "main",
+                   at = @At(value = "INVOKE",
+                            target = "Lnet/minecraft/world/level/storage/LevelStorageSource$LevelStorageAccess;saveDataTag(Lnet/minecraft/core/RegistryAccess;Lnet/minecraft/world/level/storage/WorldData;)V"
+                   )
+    )
+    private static void servux_onCaptureImmutable(LevelStorageSource.LevelStorageAccess instance, RegistryAccess registryAccess, WorldData worldData, Operation<Void> original)
     {
-        DataProviderManager.INSTANCE.onCaptureImmutable(frozen);
-        DataProviderManager.INSTANCE.onCaptureRootDir(settingsFile);
+        DataProviderManager.INSTANCE.onCaptureImmutable((RegistryAccess.Frozen) registryAccess);
+        DataProviderManager.INSTANCE.onCaptureRootDir(Paths.get("server.properties"));
+        original.call(instance, registryAccess, worldData);
     }
 }
