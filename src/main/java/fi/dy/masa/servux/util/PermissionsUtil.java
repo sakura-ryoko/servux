ee715dcfeb5df9f9111e85248668db5bf2013fb3
Sakura Ryoko <sakura@ryoko.soy>
2026-09-21 20:35:09 -0400

    fix: add permissions node sanitization

--- src/main/java/fi/dy/masa/servux/util/PermissionsUtil.java
+++ src/main/java/fi/dy/masa/servux/util/PermissionsUtil.java
@@ -11,10 +11,12 @@ import net.minecraft.world.entity.Entity;
 
 public class PermissionsUtil
 {
+	public static final String REGEX_ALLOWED = "[^a-z0-9:_./\\-]+";        // Identifier Safe
+
 	public static boolean require(CommandSourceStack ctx, @NotNull String node, int level)
 	{
 		PermissionLevel pl = PermissionLevel.byId(Mth.clamp(level, 0, PermissionLevel.OWNERS.id()));
-		Identifier id = Identifier.parse(node);
+		Identifier id = Identifier.parse(sanitizeNode(node));
 
 		if (ctx.isPlayer() && ctx.getPlayer() != null)
 		{
@@ -27,7 +29,12 @@ public class PermissionsUtil
 	public static boolean check(Entity entity, @NotNull String node, int level)
 	{
 		PermissionLevel pl = PermissionLevel.byId(Mth.clamp(level, 0, PermissionLevel.OWNERS.id()));
-		Identifier id = Identifier.parse(node);
+		Identifier id = Identifier.parse(sanitizeNode(node));
 		return entity.checkPermission(id, pl);
+	}
+
+	public static String sanitizeNode(@NotNull final String node)
+	{
+		return node.toLowerCase().replaceAll(REGEX_ALLOWED, "");
 	}
 }
